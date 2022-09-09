aba02c3dea1ad21a38028aceee3d2b97f05bd39e
NhatNgo97 <nhatngo9799@gmail.com>
2022-09-09 11:59:40 +1000

    nav bar

--- src/components/layout/NavBar/index.js
+++ src/components/layout/NavBar/index.js
@@ -1,21 +1,22 @@
 import { IoIosArrowBack } from "react-icons/io";
 import { BsMoon } from "react-icons/bs";
+import IconButton from "../../common/IconButton";
 
 function NavBar() {
   return (
-    <div className="h-[50px] border-b-2 border-slate-500 flex flex-row justify-between items-center px-4">
+    <div className="h-[50px] border-b-2 border-slate-500 flex flex-row justify-between items-center px-4 flex-shrink-0">
       <div>
-        <button>
+        <IconButton>
           <IoIosArrowBack />
-        </button>
+        </IconButton>
       </div>
       <div>
         <h2>IContacts</h2>
       </div>
       <div>
-        <button>
+        <IconButton>
           <BsMoon />
-        </button>
+        </IconButton>
       </div>
     </div>
   );
