066e0580572fc1f6e9f74d83d20d228a2e4e4720
NhatNgo97 <nhatngo9799@gmail.com>
2022-09-09 21:08:49 +1000

    update break point on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      mdp: "850px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {
       backgroundImage: {
         search: "url(https://static.thenounproject.com/png/101791-200.png)",
