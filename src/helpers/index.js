5ecf27d0e5d656ad3d8e4a2ce2df1afcecb513e0
NhatNgo97 <nhatngo9799@gmail.com>
2022-09-09 21:09:08 +1000

    format consistent phone number

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -6,6 +6,18 @@ const helper = {
     });
     return obj;
   },
+  formatPhoneNumber(phone) {
+    //normalize string and remove all unnecessary characters
+    phone = phone.replace(/[^\d]/g, "");
+
+    //check if number length equals to 10
+    if (phone.length == 10 || 11) {
+      //reformat and return phone number
+      return phone.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3");
+    }
+
+    return phone;
+  },
 };
 
 export default helper;
